ed5d9e2385ebdbef4245f6ad298aae1772362531
SultanGoated <111977953+SultanGoated@users.noreply.github.com>
2022-11-28 07:48:00 +0000

    Deploying to gh-pages from @ SultanGoated/SultanGoated.github.io@185620a5d0df55e8b69ab5e87711b6edebe8eb2f 🚀

---
+++ _app/immutable/chunks/2-7fe8fbf2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-1eac4987.js";export{t as component};
---
+++ _app/immutable/chunks/4-9076d9d2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/todolist/_page.svelte-83526cfc.js";export{t as component};
