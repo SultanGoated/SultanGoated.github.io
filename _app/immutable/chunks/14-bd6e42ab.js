6c485d2000a623b522a0f9dbaf948ad8d90c9139
SultanGoated <111977953+SultanGoated@users.noreply.github.com>
2023-06-09 17:20:52 +0000

    Deploying to gh-pages from @ SultanGoated/SultanGoated.github.io@cd3598bbf1ca6383a2dbef5814d192baffcb6d34 🚀

---
+++ _app/immutable/chunks/14-bd6e42ab.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/sooriSpecial/_page.svelte-f995699b.js";export{t as component};
--- _app/immutable/chunks/14-d108960c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/sooriSpecial/_page.svelte-11dc6d91.js";export{t as component};
